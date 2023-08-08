e34f82438041bde3aafd5d096c7c083702c73a11
ryzhay <elenaaltukhova1@gmail.com>
2023-08-08 21:25:53 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+const itemsMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class='gallery__item'>
+   <a class='gallery__link' href='${original}'>
+      <img class='gallery__image' 
+      src='${preview}' 
+      alt='${description}' />
+   </a>
+</li>`;
+    }).join('');
+};
+
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captions: 'true',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+     });
+
 console.log(galleryItems);

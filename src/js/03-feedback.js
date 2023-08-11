2f7bfb6a3d8fe1de63ca845aa7718aa0f873a6ba
ryzhay <elenaaltukhova1@gmail.com>
2023-08-11 12:54:47 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,13 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = "feedback-form-state";
-let email = '';
-let message = '';
-
-const formData = {
-    email,
-    message,
-};
+let formData;
 
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
@@ -17,18 +11,24 @@ const currentLocalData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 if(currentLocalData){
     form.email.value = currentLocalData.email;
     form.message.value = currentLocalData.message;
+    formData = {
+        email: form.email.value,
+        message: form.message.value,
+    };
 };
 
 function onInput () {
-   email = form.email.value,
-   message = form.message.value,
-   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    formData = {
+   email: form.email.value,
+   messageValue: form.message.value,
 };
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 
+};
+ 
 function onSubmit(event){
-    event.preventDefault();
-    console.log(formData);
+    // event.preventDefault();
     localStorage.removeItem(STORAGE_KEY);
           form.reset();
-            
-}; 
+          console.log(formData);  
+};
